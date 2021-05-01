ecd70e62d0791f315209e9233df8d6a6eecaa5da
taro1025 <game4967@gmail.com>
2021-05-02 03:44:13 +0900

    change s3 to 52.193...

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
  Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3001', 'http://pua-review-frontend.s3-website-ap-northeast-1.amazonaws.com'
+    origins 'http://localhost:3001', 'http://52.193.23.14'
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env === 'production'
-    Rails.application.config.session_store :cookie_store, key: '_PuAReview', domain: 'pua-review-frontend.s3-website-ap-northeast-1.amazonaws.com'
+    Rails.application.config.session_store :cookie_store, key: '_PuAReview', domain: '52.193.23.14'
 else
     Rails.application.config.session_store :cookie_store, key: '_PuAReview'
 end
